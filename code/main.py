02ed26b8476d332acc88ac5b0065c46cbd48bf98
Federico Mengozzi <19249682+fedemengo@users.noreply.github.com>
2023-05-31 22:27:02 +0200

    fact (#3)
    * fact
    
    * lulz
    
    * fix it?

--- code/main.py
+++ code/main.py
@@ -2,6 +2,15 @@
 
 import os
 
+
+def factorial(n):
+    print('factorial({})'.format(n))
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
 if __name__ == '__main__':
     print('Hello World!')
     os.exit(0)
