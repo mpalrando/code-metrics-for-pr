c9a0c91e520c70e2dd2c4401e2df29b618f04f2e
Federico Mengozzi <19249682+fedemengo@users.noreply.github.com>
2023-06-02 11:44:39 +0200

    clean up

--- .github/workflows/sz-diff.py
+++ .github/workflows/sz-diff.py
@@ -6,29 +6,27 @@ from tabulate import tabulate
 
 if __name__ == '__main__':
   base, pr = gen_stats(sys.argv[1]), gen_stats(sys.argv[2])
-
   pr_dirs = {x[0].rsplit("/", 1)[0]: x[1] for x in pr}
   base_dirs = {x[0].rsplit("/", 1)[0]: x[1] for x in base}
-  base_ff, pr_ff = {x[0]: x for x in base}, {x[0]: x for x in pr}
-  total_base, total_pr = sum(x[1] for x in base), sum(x[1] for x in pr)
+  base_files, pr_files = {x[0]: x for x in base}, {x[0]: x for x in pr}
+  base_loc, pr_loc = sum(x[1] for x in base), sum(x[1] for x in pr)
 
   headers = ["File", "Lines", "Tokens/Line", "Diff", ""]
   files = []
   for x in pr:
-    if x[0] in base_ff:
-      diff = x[1]-base_ff[x[0]][1]
+    if x[0] in base_files:
+      diff = x[1]-base_files[x[0]][1]
       files.append([x[0], x[1], x[2], f'{diff:+}' if diff != 0 else "", "M" if diff != 0 else ""])
     else:
       files.append([x[0], x[1], x[2], f'{x[1]:+}', "A"])
-  deleted = [[x[0], x[1], x[2], f'{-x[1]:+}', "D"] for x in base if x[0] not in pr_ff]
+  deleted = [[x[0], x[1], x[2], f'{-x[1]:+}', "D"] for x in base if x[0] not in pr_files]
   files += deleted
-
-  print(tabulate([headers] + files, headers="firstrow", floatfmt=".1f", colalign=("left", "right", "right", "right", "right"))+"\n")
 
   dirs = []
   for dir_name, group in sorted(pr_dirs.items(), key=lambda x: -x[1]):
     diff = group-base_dirs.get(dir_name, 0)
     dirs.append([dir_name, group, f'{diff:+}' if diff != 0 else ""])
 
+  print(tabulate([headers] + files, headers="firstrow", floatfmt=".1f", colalign=("left", "right", "right", "right", "right"))+"\n")
   print(tabulate(dirs, headers=["Dir", "Lines", "Diff"], floatfmt=".1f")+"\n")
-  print(f"total line count: {total_pr} ({total_pr-total_base:+})")
+  print(f"total line count: {pr_loc} ({pr_loc-base_loc:+})")
