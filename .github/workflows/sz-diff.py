34cd9e4fba9c4d14cb65634f61aef7fee371a052
Federico Mengozzi <19249682+fedemengo@users.noreply.github.com>
2023-06-28 22:01:11 +0200

    simplify

--- .github/workflows/sz-diff.py
+++ .github/workflows/sz-diff.py
@@ -5,23 +5,16 @@ from sz import gen_stats, FileStats
 from tabulate import tabulate
 
 
-class Ref:
-  def __init__(self, files: list[FileStats]):
-    self.files = files
-    self.files_set = dict({f.name: f for f in files})
-    self.dirs_set = dict({f.dir: f for f in files})
-    self.total_lc = sum(f.lines_count for f in files)
-
-  def file(self, name: str):
-    return self.files_set.get(name, None)
-
-  def diff(self, other: 'Ref', unchanged=False):
-    return RefDiff(self, other, unchanged=unchanged)
-
-
 class RefDiff:
-  def __init__(self, base: Ref, other: Ref, unchanged=False):
-    self.base, self.other = base, other
+  def __init__(self, base: list[FileStats], other: list[FileStats], unchanged=False):
+    self.base_files, self.other_files = base, other
+
+    self.base_files_set = dict({f.name: f for f in base})
+    self.base_total_lc = sum(f.lines_count for f in base)
+
+    self.other_files_set = dict({f.name: f for f in other})
+    self.other_total_lc = sum(f.lines_count for f in other)
+
     self._modified = self.modified(unchanged)
     self._added = self.added()
     self._deleted = self.deleted()
@@ -35,40 +28,38 @@ class RefDiff:
                + self.format(self._deleted, "D"))
     return tabulate(changes, headers="keys", floatfmt=".1f", colalign=("left",) + ("right",) * 4)
 
-  def file_lc_diff(self, name: str):
-    assert name in self.base.files_set, f"file {name} not found"
-    base_lc = self.base.file(name).lines_count
-    other_lc = self.other.file(name).lines_count
+  def files_line_count_diff(self, name: str):
+    base_lc = self.base_files_set.get(name).lines_count
+    other_lc = self.other_files_set.get(name).lines_count
     return base_lc - other_lc
 
   def modified(self, unchanged):
     files = []
-    for f in self.base.files:
-      if f.name in self.other.files_set:
-        lc_diff = self.file_lc_diff(f.name)
-        if unchanged or lc_diff != 0:
-          files.append({**f.format(), **{"diff": lc_diff}})
+    for f in self.base_files:
+      if f.name in self.other_files_set:
+        diff = self.files_line_count_diff(f.name)
+        if unchanged or diff != 0:
+          files.append({**f.format(), **{"diff": diff}})
     return files
 
   def added(self):
     files = []
-    for f in self.other.files:
-      if f.name not in self.base.files_set:
+    for f in self.other_files:
+      if f.name not in self.base_files_set:
         files.append({**f.format(), **{"diff": f.lines_count}})
     return files
 
   def deleted(self):
     files = []
-    for f in self.base.files:
-      if f.name not in self.other.files_set:
+    for f in self.base_files:
+      if f.name not in self.other_files_set:
         files.append({**f.format(), **{"diff": -f.lines_count}})
     return files
 
 
 if __name__ == '__main__':
   base, pr = gen_stats(sys.argv[1]), gen_stats(sys.argv[2])
-  base_ref, pr_ref = Ref(base), Ref(pr)
-  diff = base_ref.diff(pr_ref, unchanged=False)
+  diff = RefDiff(base, pr, unchanged=False)
 
   print(diff.changes_table(), "\n")
-  print(f"total line count: {pr_ref.total_lc} ({pr_ref.total_lc-base_ref.total_lc:+})")
+  print(f"total line count: {diff.base_total_lc} ({diff.other_total_lc-diff.base_total_lc:+})")
