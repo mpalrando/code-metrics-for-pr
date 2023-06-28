3b8b19da3859102076a8368bb171a75359ad2cc3
Federico Mengozzi <19249682+fedemengo@users.noreply.github.com>
2023-06-28 22:32:09 +0200

    easter egg

--- .github/workflows/sz-diff.py
+++ .github/workflows/sz-diff.py
@@ -6,14 +6,14 @@ from tabulate import tabulate
 
 
 class RefDiff:
-  def __init__(self, base: list[FileStats], other: list[FileStats], unchanged=False):
-    self.base_files, self.other_files = base, other
+  def __init__(self, base: list[FileStats], pr: list[FileStats], unchanged=False):
+    self.base_files, self.pr_files = base, pr
 
     self.base_files_set = dict({f.name: f for f in base})
     self.base_total_lc = sum(f.lines_count for f in base)
 
-    self.other_files_set = dict({f.name: f for f in other})
-    self.other_total_lc = sum(f.lines_count for f in other)
+    self.pr_files_set = dict({f.name: f for f in pr})
+    self.pr_total_lc = sum(f.lines_count for f in pr)
 
     self._modified = self.modified(unchanged)
     self._added = self.added()
@@ -29,14 +29,12 @@ class RefDiff:
     return tabulate(changes, headers="keys", floatfmt=".1f", colalign=("left",) + ("right",) * 4)
 
   def files_line_count_diff(self, name: str):
-    base_lc = self.base_files_set.get(name).lines_count
-    other_lc = self.other_files_set.get(name).lines_count
-    return base_lc - other_lc
+    return self.base_files_set.get(name).lines_count - self.pr_files_set.get(name).lines_count
 
   def modified(self, unchanged):
     files = []
     for f in self.base_files:
-      if f.name in self.other_files_set:
+      if f.name in self.pr_files_set:
         diff = self.files_line_count_diff(f.name)
         if unchanged or diff != 0:
           files.append({**f.format(), **{"diff": diff}})
@@ -44,7 +42,7 @@ class RefDiff:
 
   def added(self):
     files = []
-    for f in self.other_files:
+    for f in self.pr_files:
       if f.name not in self.base_files_set:
         files.append({**f.format(), **{"diff": f.lines_count}})
     return files
@@ -52,7 +50,7 @@ class RefDiff:
   def deleted(self):
     files = []
     for f in self.base_files:
-      if f.name not in self.other_files_set:
+      if f.name not in self.pr_files_set:
         files.append({**f.format(), **{"diff": -f.lines_count}})
     return files
 
@@ -62,4 +60,8 @@ if __name__ == '__main__':
   diff = RefDiff(base, pr, unchanged=False)
 
   print(diff.changes_table(), "\n")
-  print(f"total line count: {diff.base_total_lc} ({diff.other_total_lc-diff.base_total_lc:+})")
+  print(f"total line count: {diff.base_total_lc} ({diff.pr_total_lc-diff.base_total_lc:+})")
+
+  if diff.pr_total_lc < diff.base_total_lc:
+    sys.exit(1)
+  sys.exit(0)
